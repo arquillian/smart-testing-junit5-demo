4dc75bd8818171e751138d2bd2943126cf6bfe40
Alex Soto <asotobu@gmail.com>
2017-10-23 11:32:47 +0200

    Initial Commit

---
+++ src/test/java/junit5/FirstTest.java
@@ -0,0 +1,16 @@
+package junit5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Fist Test")
+public class FirstTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
